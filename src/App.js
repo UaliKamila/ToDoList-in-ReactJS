e76f121e56d43bdab26c06bfc0f4f38334011712
Kamila <ualikamila@gmail.com>
2021-08-26 18:21:02 +0600

    Сделала добавление задач. Добавила catch для оповещений об ошибках запросов

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { List, AddList, Tasks } from './components';
 function App() {
   const [lists, setLists] = useState(null);
   const [colors, setColors] = useState(null);
+  const [activeItem, setActiveItem] = useState(null);
+
 
   useEffect(() => { //useEffect дает понять отрендерилось ли компонент App
     axios.get('http://localhost:3001/lists?_expand=color&_embed=tasks').then(({ data }) => {
@@ -21,11 +23,32 @@ function App() {
       setLists(newList);
     };
 
+    const onAddTask = (listId, taskObj) => {
+      const newList = lists.map(item => {
+        if (item.id === listId) {
+          item.tasks = [...item.tasks, taskObj];
+        }
+        return item;
+      });
+      setLists(newList);
+    };
+
+    const onEditListTitle = (id, title) => {
+      const newList = lists.map(item => {
+        if (item.id === id) {
+          item.name = title;
+        }
+        return item;
+      })
+      setLists(newList);
+    }
+
   return (
     <div className="todo">
       <div className="todo__sidebar">
        <List items={[
          {
+           active: true,
            icon: <svg 
 						width="14" 
 						height="12" 
@@ -46,6 +69,10 @@ function App() {
               const newLists = lists.filter(item => item.id !== id);
               setLists(newLists);
             }}
+            onClickItem={item => {
+              setActiveItem(item);
+            }}
+            activeItem={activeItem}
             isRemovable 
           />
        ) : (
@@ -53,7 +80,11 @@ function App() {
        )}
       <AddList onAdd={onAddList} colors={colors} />
       </div>
-      <div className="todo__tasks">{lists && <Tasks list={lists[1]} />}</div>
+      <div className="todo__tasks">
+        {lists && activeItem && (
+          <Tasks list={activeItem} onAddTask={onAddTask} onEditTitle={onEditListTitle}/>
+        )}
+      </div>
     </div>
   );
 }
